20b9a38878bbd4cfb53b3513eac2d04d3eb73f1c
anyaaSegal <148961364+anyaaSegal@users.noreply.github.com>
2023-10-25 21:31:39 +0000

    form done still need to finish rest of contact.jsx

--- my-admin/src/Contact.jsx
+++ my-admin/src/Contact.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {useRef} from 'react';
 
 export default function Contact() {
+    const form = useRef()
+
+    const sendEmail = () => {}; // still need to finish function and Email.js
   return (
-    <div>
-        Contact
-    </div>
+    <section>
+      <div className='contact-container'>
+        <h2 className='text-center'> Contact Us </h2>
+        <form ref={form} onSubmit={sendEmail} className='form-control-card'>
+          <input type='text' placeholder='Full Name' name='user_name' required />
+          <input type='email' placeholder='Email' name='user_email' required />
+          <input type='text' placeholder='Subject' name='subject' required />
+          <textarea name='message' placeholder="Penny for your thoughts..." cols='30' rows='10'></textarea>
+          <button type='submit' className='button'> Send Message </button>
+        </form>
+      </div>
+    </section>
   )
 };
